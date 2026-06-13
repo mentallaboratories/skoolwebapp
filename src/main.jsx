eda7acc5c9b3d0865420ff21260f780b55d3fb4d
mentallaboratories <93845562+mentallaboratories@users.noreply.github.com>
2026-06-13 08:30:46 +0300

    github to show site react

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';   // ← rename your main.css to index.css, or change this path
 import App from './App';
 
+import { BrowserRouter } from 'react-router-dom';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter basename="/skoolwebapp">
     <App />
+    </BrowserRouter>
   </StrictMode>
 );
+
+
